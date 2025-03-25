4af72276caec14f9ca293a2eecbb0acdbac9b263
Ayelid <aarevaloq@ufpso.edu.co>
2025-03-25 07:14:23 -0500

    Parcial

--- src/codigoOptimizado.java
+++ src/codigoOptimizado.java
@@ -12,30 +12,32 @@ public class codigoOptimizado {
         double suma = 0;
 
         for(int i = 0; i<=productos; i++){
+            input.nextLine();
+
             System.out.println("Escriba el nombre del producto:");
             String nomProducto = input.nextLine();
-            input.nextLine();
+            
             System.out.println("Ingrese el precio:");
             double precio = input.nextDouble();
+
             System.out.println("Cuantos productos vas a llevar: ");
             double cantidad = input.nextDouble();
 
-            cantidad = 0;
-            precio = cantidad * precio;
             
+            precio *= cantidad;
             double impuesto = precio * porcentajeImpuesto;
+
+            double total = precio + impuesto;
 
             if (precio > 10000) {
                 double descuento = (precio *  porcentajeDescuento);
-                double total = precio + impuesto - descuento;
+                total -= descuento;
                 System.out.println("Producto: " + nomProducto + ", Precio final: " + total);
-                suma += total;
             } else {
                 System.out.println("No tiene descuento: ");
-                double total = precio + impuesto ;
                 System.out.println("Producto: " + nomProducto + ", Precio final: " + total);
-                suma += total;
             }
+            suma += total;
         }
         System.out.println("El total de la compra es: " + suma);
         input.close();
